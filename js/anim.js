f1c7c77881c1cdb0648ff97265a4bb8df0a6661c
Caroline Gryglas <Caroline@Carolines-MBP.lan>
2022-09-23 15:07:54 +0200

    nyanimation

---
+++ js/anim.js
@@ -0,0 +1,19 @@
+HTML CSS JSResult Skip Results Iframe
+EDIT ON
+var words = ['Hi i like HTML', 'I also like css', 'Lorem ipsum dolor sit amet', ' consectetur adipiscing elit', 'sed do eiusmod tempor incididunt'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay){
+          forwards = false;
+          skip_count = 0;}
